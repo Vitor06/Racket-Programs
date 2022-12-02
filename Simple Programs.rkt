cb412258c6140ac45ef64d7724651fdf8399e10a
Vitor Fernandes <90469090+Vitor06@users.noreply.github.com>
2022-12-02 19:54:57 -0300

    Update Simple Programs.rkt

--- Simple Programs.rkt
+++ Simple Programs.rkt
@@ -1,15 +1,13 @@
 #lang racket
-;;1 - Defina uma função que encontre o maior valor entre 2 valores dados.
+;;Defina uma função que encontre o maior valor entre 2 valores dados.
 (
  define (maior a b)
   (
    if (> a b) a b
   )
 )
-
-;;2 - Defina uma função que receba 3 n ́umeros como parâmetros e retorne a soma dos quadrados
+;;Defina uma função que receba 3 n ́umeros como parˆametros e retorne a soma dos quadrados
 ;;dos dois maiores numeros.
-
 (define (somaQuadrado a b)
   (+ (* a a)(* b b))
   )
@@ -23,16 +21,15 @@
   )
 
   )
-;;3 - Defina uma funcao que calcule a distância de um ponto no plano cartesiano (representado por dois
+;;Defina uma fun ̧c ̃ao que calcule a distˆancia de um ponto no plano cartesiano (representado por dois
 ;;numeros) a origem.
 
 (define ( distancia x y x-origem y-origem  )
    (sqrt ( + (expt (- x-origem x) 2) (expt (- y-origem y) 2)))
 )
-
-;;4 - Defina uma funcao que receba como parâmetro 3 numeros que representam os lados de um triângulo
-;;e classifique o triângulo como equilátero, isosceles ou escaleno. Veja a pagina sobre triângulos na
-;;Wikip ́edia.
+;;Defina uma funcao que receba como parˆametro 3 numeros que representam os lados de um triˆangulo
+;;e classifique o triˆangulo como equil ́atero, isosceles ou escaleno. Veja a pagina sobre triˆangulos na
+;;Wikipédia.
 
 (define (classificaTriangulo a b c)
   (cond [(= a b c) print "Triângulo equilátero"]
@@ -41,5 +38,20 @@
 
         )
 )
-(define (IMC peso altura ))
+;;Defina uma funcao que classifique o grau de obesidade de uma pessoa usando o IMC.
+;;Altura em cm
+
+(define (IMC peso altura )
+ 
+  (define imc (* (/ peso (expt altura 2)) 10000) )
+  
+  (cond[(< imc 18.5) (string-append "imc - " (~v (exact->inexact imc))  " -> peso baixo")]
+       [(and (> imc 18.5)(< imc 24.9)) print (string-append "imc - " (~v (exact->inexact imc)) " -> peso normal")]
+       [(and (> imc 25.0)(< imc 29.9)) print (string-append "imc - " (~v (exact->inexact imc)) " -> sobre peso")]
+       [(and (> imc 30.0)(< imc 34.9)) print (string-append "imc - " (~v (exact->inexact imc)) " -> obesidade grau I")]
+       [(and (> imc 35.0)(< imc 39.9)) print (string-append "imc - " (~v (exact->inexact imc)) " -> obesidade grau II")]
+       [else  print (string-append "imc - " (~v (exact->inexact imc)) " - obesidade grau III ou mórbida")]
+   )
+
+  )
 
